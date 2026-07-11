dc9d654f80e6e2ec77d29c97d38186a6c25c815e
brianlarracas-ctrl <brian.larracas@gmail.com>
2026-07-11 15:06:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+// Fade-in animation
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+    });
+});
 
+document.querySelectorAll("section").forEach(section=>{
+    section.classList.add("hidden");
+    observer.observe(section);
+});
+
+// Current year in footer
+const footer = document.querySelector("footer p");
+footer.innerHTML = `© ${new Date().getFullYear()} JustInLink. All Rights Reserved.`;
